c492a2e73c4672f548ecdde051b772fe0fad77eb
Minwei Xu <faceswilliam@gmail.com>
2016-05-10 20:42:27 -0700

    import

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,6 +1,6 @@
 //Including dependency
 var Sequelize = require("sequelize");
- 
+
 //Setting up the config
 var sequelize = new Sequelize('crunchbase', 'root', null, {
   host: 'localhost',
@@ -12,6 +12,18 @@ var sequelize = new Sequelize('crunchbase', 'root', null, {
     idle: 10000
   }
 });
+
+var Acquisition = sequelize.import(__dirname + "/models/cb_acquisitions");
+var Degree = sequelize.import(__dirname + "/models/cb_degrees");
+var Funding_round = sequelize.import(__dirname + "/models/cb_funding_rounds");
+var Fund = sequelize.import(__dirname + "/models/cb_funds");
+var Investment = sequelize.import(__dirname + "/models/cb_investments");
+var IPO = sequelize.import(__dirname + "/models/cb_ipos");
+var Milestone = sequelize.import(__dirname + "/models/cb_milestones");
+var Object = sequelize.import(__dirname + "/models/cb_objects");
+var Office = sequelize.import(__dirname + "/models/cb_offices");
+var People = sequelize.import(__dirname + "/models/cb_people");
+var Relationship = sequelize.import(__dirname + "/models/cb_relationships");
 
 sequelize
   .authenticate()
