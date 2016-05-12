a27241e8df98d9173100b39c399b73981eae0606
Minwei Xu <faceswilliam@gmail.com>
2016-05-12 16:18:41 -0700

    refactor

--- graphql-server/db.js
+++ graphql-server/db.js
@@ -62,54 +62,6 @@ Object.Relationships = Object.hasMany(Relationship, {
   foreignKey: 'relationship_object_id',
 });
 
-// Sequelize test
-Degree
-  .findOne({
-    where: { object_id: 'p:3262' },
-    include: [{ model: People, as: 'people' }],
-  })
-  .then((degree) => {
-    console.log(degree.people.get('first_name') + ' ' + degree.people.get('last_name'));
-  });
-
-People
-  .findOne({
-    where: { object_id: 'p:3262' },
-    include: [{ model: Degree, as: 'degrees' }],
-  })
-  .then((people) => {
-    people.degrees.map((d) => {
-      console.log(d.get('institution'));
-    });
-  });
-
-Relationship
-  .findAll({
-    where: { person_object_id: 'p:3262' },
-    include: [
-      { model: Object, as: 'object' },
-      { model: People, as: 'people' },
-    ],
-  })
-  .then((relationships) => {
-    relationships.map((r) => {
-      if (r.object) {
-        console.log(r.people.get('last_name') + ' works at ' + r.object.get('name'));
-      }
-    });
-  });
-
-Object
-  .findOne({
-    where: { id: 'c:1' },
-    include: [{ model: Relationship, as: 'relationship' }],
-  })
-  .then((people) => {
-    people.relationship.map((d) => {
-      console.log(d.get('title'));
-    });
-  });
-
 module.exports = {
   Acquisition,
   Degree,
